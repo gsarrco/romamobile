0e9594bdffb956abc13d334237f2a877e2bf8b0c
luca <luca@b4c076cd-ec08-490b-a76a-ed55b41f7846>
2013-12-06 09:04:01 +0000

    Merged revision(s) 3774 from trunk/atacmobile: * Paline: bugfix: corretto ordine scaricamento rete

--- paline/management/commands/scarica_dati_tpl.py
+++ paline/management/commands/scarica_dati_tpl.py
@@ -18,15 +18,15 @@ class Command(BaseCommand):
 	
 
 	def handle(self, *args, **options):
-		print "\n\n1 di 5: Download rete"
-		scarica_rete()
-		print "\n\n2 di 5: Aggiornamento rete nel database"
-		carica_rete()
-		print "\n\n3 di 5: Download orari partenza"
+		print "\n\n1 di 5: Download orari partenza"
 		scarica_orari_partenza()
-		print "\n\n4 di 5: Ricalcolo frequenze da timetable"
+		print "\n\n2 di 5: Ricalcolo frequenze da timetable"
 		calcola_frequenze()
-		print "\n\n5 di 5: Download statistiche passaggi"
+		print "\n\n3 di 5: Download statistiche passaggi"
 		scarica_stat_passaggi()		
-		
+		print "\n\n4 di 5: Download rete"
+		scarica_rete()
+		print "\n\n5 di 5: Aggiornamento rete nel database"
+		carica_rete()
+
 		
